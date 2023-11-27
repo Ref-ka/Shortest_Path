d6cba10cbb6830a779ec65ccea0553371c09a637
Ref_ka <228refka1337@gmail.com>
2023-11-27 10:34:25 +0500

    add the prefix 'info' for back functions with info object

--- backend.py
+++ backend.py
@@ -16,13 +16,13 @@ class Back:
         self.view.start_main_loop()
 
 # Info methods
-    def connection_check(self, var) -> bool:
+    def info_connection_check(self, var) -> bool:
         return self.info.connection_check(var)
 
-    def insert_connection(self, var):
+    def info_insert_connection(self, var):
         self.info.insert_connection(var)
 
-    def clear(self):
+    def info_clear(self):
         self.info.clear()
         self.view.clear()
 
@@ -32,29 +32,29 @@ class Back:
     def get_answer_wfi(self):
         self.view.get_answer(self, True)
 
-    def delete_connection(self, var):
+    def info_delete_connection(self, var):
         self.info.delete_connection(var)
 
-    def get_vertexes(self) -> list:
+    def info_get_vertexes(self) -> list:
         return self.info.get_vertexes()
 
-    def get_vertexes_count(self) -> int:
+    def info_get_vertexes_count(self) -> int:
         return self.info.get_vertexes_count()
 
-    def insert_src(self, src):
+    def info_insert_src(self, src):
         self.info.insert_src(src)
 
-    def get_connections(self) -> list:
+    def info_get_connections(self) -> list:
         return self.info.get_connections()
 
-    def get_src(self) -> int:
+    def info_get_src(self) -> int:
         return self.info.get_src()
 
 # View methods
     def load_file(self):
         file_path = filedialog.askopenfilename()
         if file_path:
-            self.clear()
+            self.info_clear()
             with open(file_path, 'r') as file:
                 start = time.monotonic()
                 data = file.readlines()
@@ -75,11 +75,11 @@ class Back:
     def make_graph(self, wfi) -> tuple:
         draw = self.view.draw_switch.get()
         #  Создаем объект класса Graph и получаем ответ
-        g = Graph(self.get_vertexes_count())
-        for line in self.get_connections():
+        g = Graph(self.info_get_vertexes_count())
+        for line in self.info_get_connections():
             g.add_edge(line[0], line[1], line[2])
         if not wfi:
-            return g.spfa(int(self.get_src()), draw)
+            return g.spfa(int(self.info_get_src()), draw)
         else:
             return g.wfi(draw)
 
