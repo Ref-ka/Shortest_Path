b9cb91737f438f8e21f0029ca6d20a58f74d3aaa
Ref_ka <228refka1337@gmail.com>
2023-11-30 11:32:34 +0500

    Some new comments

--- info_class.py
+++ info_class.py
@@ -20,7 +20,7 @@ class Info:
     def insert_connection(self, line):
         self.connections.append(line)
         for i in range(2):
-            if line[i] not in self.vertexes:
+            if line[i] not in self.vertexes:  # Counter of vertexes
                 self.vertexes[line[i]] = 1
             else:
                 self.vertexes[line[i]] += 1
@@ -28,22 +28,22 @@ class Info:
     def delete_connection(self, line):
         self.connections.remove(line)
         for i in range(2):
-            if self.vertexes[line[i]] > 1:
+            if self.vertexes[line[i]] > 1:  # Change vertex counter when delete some connection
                 self.vertexes[line[i]] -= 1
             else:
-                self.vertexes.pop(line[i])
+                self.vertexes.pop(line[i])  # If amount of vertex equals 1 we delete this vertex from dict
 
     def insert_src(self, src):
         self.src = src
 
     def connection_check(self, line) -> tuple:
         for connection in self.connections:
-            if line[:2] == connection[:2]:
+            if line[:2] == connection[:2]:  # Check if inserting line already was inserted but with another weight
                 if line[2] == connection[2]:
-                    return True, False
+                    return True, False  # Same connection already exist
                 else:
-                    return True, True
-        return False, False
+                    return True, True  # Connection already exist but with another weight
+        return False, False  # Connection doesn't exist
 
     def clear(self):
         self.connections = []
